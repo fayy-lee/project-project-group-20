62676658e0a9c636c30a7d05a3c963d82d81b141
Chelsea <chelsea.ren.b@gmail.com>
2023-10-05 16:31:18 -0400

    implemented Admin class

---
+++ Admin.java
@@ -0,0 +1,20 @@
+public class Admin extends User{
+
+    //one specific set of Admin credentials
+    private static String userName = "Admin20";
+    private static String passWord = "AdminPass123";
+
+    public Admin() {
+        super(userName, passWord);
+    }
+   
+    public String getUser(){
+        return userName;
+    }
+
+    public String getPass(){
+        return passWord;
+    }
+
+    
+}
