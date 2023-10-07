4b6059afb28821b1f4ba3ae1009d47c0ffa5bfff
Chelsea <chelsea.ren.b@gmail.com>
2023-10-07 12:59:31 -0400

    created doctor and patient files

---
+++ Doctor.java
@@ -0,0 +1,8 @@
+public class Doctor extends User{
+
+    public Doctor(String user, String pass) {
+        super(user, pass);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ Patient.java
@@ -0,0 +1,8 @@
+public class Patient extends User{
+
+    public Patient(String user, String pass) {
+        super(user, pass);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
