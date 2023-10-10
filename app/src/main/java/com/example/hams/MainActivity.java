ab7525f3329e6186528214fd882e75c6768ee425
Chelsea <chelsea.ren.b@gmail.com>
2023-10-10 08:41:18 -0400

    created Android Studio project and connected FireBase

---
+++ app/src/main/java/com/example/hams/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.hams;
+
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    //initialize the databasse connection and the references (2 so that we don't just override one node
+    FirebaseDatabase database;
+    DatabaseReference rootRef, newRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+        rootRef = database.getReference();
+    }
+}
