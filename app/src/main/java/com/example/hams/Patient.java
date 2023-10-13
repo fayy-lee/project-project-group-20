713db25e8a5aef0484e9add6bb09e4bc0651a0fa
“Richa <richa.kewal123@gmail.com>
2023-10-13 11:25:59 -0400

    implemented patient class

--- app/src/main/java/com/example/hams/Patient.java
+++ app/src/main/java/com/example/hams/Patient.java
@@ -1,17 +1,29 @@
 package com.example.hams;
 public class Patient extends User{
 
+    string firstName;
+    string lastName;
+    string emailAddress;
+    string phoneNo;
+    string address;
+    string healthCard;
 
-    public Patient(string firstName, string lastName, string emailAddress, string password, string username, string phoneNo, string address, string healthCard){
-        super(username, password);
+    public Patient(string firstName, string lastName, string emailAddress, string pass, string user, string phoneNo, string address, string healthCard){
+        
+        //calling superclass User's constructor
+        super(user, pass); 
+
+        //initializing Patient class' objects
         this.firstName = firstName;
         this.lastName = lastName;
         this.emailAddress = emailAddress;
         this.phoneNo = phoneNo;
         this.address = address;
         this.healthCard = healthCard;
+
+        //will be used in the future when seeking approval from Admin
         boolean active = true;
     }
     
-    
+
 }
