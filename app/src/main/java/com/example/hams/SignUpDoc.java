dbc6fb76625ac252fcd4a09b7e5ca012762bae3d
sarra2108 <ssass058@uottawa.ca>
2023-10-29 15:53:17 -0400

    fields validation

--- app/src/main/java/com/example/hams/SignUpDoc.java
+++ app/src/main/java/com/example/hams/SignUpDoc.java
@@ -1,5 +1,5 @@
 package com.example.hams;
-
+import android.text.TextUtils;
 
 
 import android.content.Intent;
@@ -38,6 +38,7 @@ public class SignUpDoc extends AppCompatActivity {
     FirebaseUser fUser;
     FirebaseDatabase database = MainActivity.database;
     DatabaseReference usersRef = MainActivity.usersRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -70,41 +71,116 @@ public class SignUpDoc extends AppCompatActivity {
                 String address = String.valueOf(add.getText());
                 String specialties = String.valueOf(special.getText());
                 //submit registration info
+                clearErrors();
+
+                // Perform validations
+                if (firstname.isEmpty() || lastname.isEmpty() || phoneNo.isEmpty() || employeeNo.isEmpty() || address.isEmpty() || emailAddress.isEmpty() || specialties.isEmpty()) {
+                    // Show error message if any field is empty
+                    showInputError("All fields are required");
+                } else if (!isValidName(firstname)) {
+                    // Show error message if first name contains non-alphabetic characters
+                    showInputError("Invalid first name");
+                } else if (!isValidName(lastname)) {
+                    // Show error message if last name contains non-alphabetic characters
+                    showInputError("Invalid last name");
+                } else if (phoneNo.length() != 10 || !TextUtils.isDigitsOnly(phoneNo)) {
+                    // Show error message if phone number is not 10 digits or contains non-digit characters
+                    showInputError("Invalid phone number (should be 10 digits)");
+                } else if (employeeNo.length() != 10 || !TextUtils.isDigitsOnly(employeeNo)) {
+                    // Show error message if employee number is not 10 digits or contains non-digit characters
+                    showInputError("Invalid employee number (should be 10 digits)");
+                } else if (!isValidEmail(emailAddress)) {
+                    // Show error message if email address is invalid
+                    showInputError("Invalid email address");
+                } else {
+
+                    mAuth.createUserWithEmailAndPassword(emailAddress, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
 
 
-                mAuth.createUserWithEmailAndPassword(emailAddress, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
+                                Doctor doctor = new Doctor();
+                                doctor.setFirstName(firstname);
+                                doctor.setLastName(lastname);
+                                doctor.setAddress(address);
+                                doctor.setPhoneNumber(phoneNo);
+                                doctor.setEmployeeNumber(employeeNo);
+                                doctor.setSpecialties(specialties);
+                                doctor.setUserName(emailAddress);
+                                doctor.setPassWord(password);
+                                doctor.setStatus("Pending");
+                                doctor.setType("Doctor");
 
+                                fUser = mAuth.getCurrentUser();
+                                String IDstring = fUser.getUid();
 
-                            Doctor doctor = new Doctor();
-                            doctor.setFirstName(firstname);
-                            doctor.setLastName(lastname);
-                            doctor.setAddress(address);
-                            doctor.setPhoneNumber(phoneNo);
-                            doctor.setEmployeeNumber(employeeNo);
-                            doctor.setSpecialties(specialties);
-                            doctor.setUserName(emailAddress);
-                            doctor.setPassWord(password);
-                            doctor.setStatus("Pending");
-                            doctor.setType("Doctor");
+                                usersRef.child(IDstring).setValue(doctor);
 
-                            fUser = mAuth.getCurrentUser();
-                            String IDstring = fUser.getUid();
+                                Toast.makeText(SignUpDoc.this, "Authentication successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(SignUpDoc.this, "Authentication failed", Toast.LENGTH_LONG).show();
 
-                            usersRef.child(IDstring).setValue(doctor);
-
-                            Toast.makeText(SignUpDoc.this, "Authentication successful", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(SignUpDoc.this, "Authentication failed", Toast.LENGTH_LONG).show();
-
+                            }
                         }
-                    }
-                });
-                openLogin();
+                    });
+                    openLogin();
+                }
+            }
+            private void showInputError(String errorMessage) {
+                // Display error message within the appropriate input field only if it's empty
+                if (first.getText().toString().trim().isEmpty()|| !isValidName(first.getText().toString().trim())) {
+                    first.setError(errorMessage);
+                }
+                if (last.getText().toString().trim().isEmpty()|| !isValidName(last.getText().toString().trim())) {
+                    last.setError(errorMessage);
+                }
+                if (phone.getText().toString().trim().isEmpty()|| phone.getText().toString().length() != 10 || !TextUtils.isDigitsOnly(phone.getText().toString())) {
+                    phone.setError(errorMessage);
+                }
+                if (employee.getText().toString().trim().isEmpty()|| special.getText().toString().length() != 10 || !TextUtils.isDigitsOnly(special.getText().toString())) {
+                    employee.setError(errorMessage);
+                }
+                if (add.getText().toString().trim().isEmpty()) {
+                    add.setError(errorMessage);
+                }
+                if (email.getText().toString().trim().isEmpty()|| !isValidEmail(email.getText().toString().trim())) {
+                    email.setError(errorMessage);
+                }
+                if (special.getText().toString().trim().isEmpty() ) {
+                    special.setError(errorMessage);
+                }
+            }
+
+            private void clearErrors() {
+                // Clear error messages from all input fields
+                first.setError(null);
+                last.setError(null);
+                phone.setError(null);
+                employee.setError(null);
+                add.setError(null);
+                email.setError(null);
+                special.setError(null);
+            }
+            private boolean isValidPhoneNumber(String phoneNumber) {
+
+                return phoneNumber.length() == 10 && TextUtils.isDigitsOnly(phoneNumber);
+            }
+
+            private boolean isValidEmail(String email) {
+
+                return !TextUtils.isEmpty(email);
+            }
+
+            private boolean isValidHealthCard(String healthCard) {
+
+                return healthCard.length() == 10 && TextUtils.isDigitsOnly(healthCard);
+            }
+            private boolean isValidName(String name) {
+                //  only alphabetic characters
+                return name.matches("[a-zA-Z]+");
             }
         });
     }
