bb52981b5bb6205d67c53c102c8b69f8a6c7111e
AR-Fatma <90813489+AR-Fatma@users.noreply.github.com>
2023-12-02 19:32:49 -0500

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/hams/ExampleUnitTest.java
+++ app/src/test/java/com/example/hams/ExampleUnitTest.java
@@ -1,17 +1,91 @@
 package com.example.hams;
-
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
+    private Doctor doctorTest;
+    private Patient patientTest;
+    private Admin adminTest;
+    private MainActivity main;
+    private UpcomingAppointments upComingA;
+    private User userToApprove;
+    private User userToReject;
+
+    @Before
+    public void init() {
+        doctorTest = new Doctor();
+        patientTest = new Patient();
+        adminTest = new Admin();
+        main = new MainActivity();
+        upComingA = new UpcomingAppointments();
+
+        doctorTest.setFirstName("John");
+        doctorTest.setLastName("Doe");
+        doctorTest.setPhoneNumber("1234567890");
+        doctorTest.setAddress("123 Main St");
+        doctorTest.setEmployeeNumber("EMP123");
+        doctorTest.setSpecialties("Cardiology");
+
+        adminTest = new Admin();
+        userToApprove = new User("UserToApprove", "Password123");
+        userToReject = new User("UserToReject", "Password456");
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void testAutoApproveAppointments() {
+        assertFalse(doctorTest.getAutoApproveAppointments()); // By default, it should be false
+
+        doctorTest.setAutoApproveAppointments(true);
+        assertTrue(doctorTest.getAutoApproveAppointments());
+    }
+
+    @Test
+    public void testUpcomingAppointments() {
+        assertTrue(doctorTest.getUpcomingAppointments().isEmpty()); // Initially, it should be empty
+
+        Appointment upcomingAppointment = new Appointment();
+        doctorTest.addUpcomingAppointment(upcomingAppointment);
+
+        assertEquals(1, doctorTest.getUpcomingAppointments().size());
+        assertTrue(doctorTest.getUpcomingAppointments().contains(upcomingAppointment));
+    }
+
+    @Test
+    public void testSetAndGetFirstName() {
+        patientTest.setFirstName("John");
+        assertEquals("John", patientTest.getFirstName());
+    }
+
+    @Test
+    public void testSetAndGetLastName() {
+        patientTest.setLastName("Doe");
+        assertEquals("Doe", patientTest.getLastName());
+    }
+
+    @Test
+    public void testSetAndGetAddress() {
+        patientTest.setAddress("123 Main St");
+        assertEquals("123 Main St", patientTest.getAddress());
+    }
+
+    @Test
+    public void testInitialization() {
+        assertEquals("Admin20", adminTest.getUser());
+        assertEquals("AdminPass123", adminTest.getPass());
+        assertEquals("Approved", adminTest.getStatus());
+    }
+
+    @Test
+    public void testApprove() {
+        adminTest.approve(userToApprove);
+        assertEquals("Approved", userToApprove.getStatus());
+    }
+
+    @Test
+    public void testReject() {
+        adminTest.reject(userToReject);
+        assertEquals("Rejected", userToReject.getStatus());
     }
 }
